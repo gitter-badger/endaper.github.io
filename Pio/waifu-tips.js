0b8ce353223cb980ece063a484a7e4c70759da7f
endaper <43871486+endaper@users.noreply.github.com>
2018-10-05 23:44:42 +0800

    Add files via upload

---
+++ Pio/waifu-tips.js
@@ -0,0 +1,128 @@
+String.prototype.render = function (context) {
+    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+
+    return this.replace(tokenReg, function (word, slash1, token, slash2) {
+        if (slash1 || slash2) {  
+            return word.replace('\\', '');
+        }
+
+        var variables = token.replace(/\s/g, '').split('.');
+        var currentObject = context;
+        var i, length, variable;
+
+        for (i = 0, length = variables.length; i < length; ++i) {
+            variable = variables[i];
+            currentObject = currentObject[variable];
+            if (currentObject === undefined || currentObject === null) return '';
+        }
+        return currentObject;
+    });
+};
+
+var re = /x/;
+console.log(re);
+re.toString = function() {
+    showMessage('哈哈，你打开了控制台，是想要看看我的秘密吗？', 5000, true);
+    return '';
+};
+
+$(document).on('copy', function (){
+    showMessage('你都复制了些什么呀?', 5000, true);
+});
+
+$(document).on('mouseout', function () {
+    showMessage('你想做什么呀？', 5000, true);
+});
+
+(function (){
+    var text;
+ 
+    var SiteIndexUrl = window.location.protocol+'//'+window.location.hostname+'/';  // 自动获取主页
+    
+    if (window.location.href == SiteIndexUrl) {      // 如果是主页
+        var now = (new Date()).getHours();
+        if (now > 23 || now <= 5) {
+            text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛';
+        } else if (now > 5 && now <= 7) {
+            text = '早上好！一日之计在于晨，美好的一天就要开始了';
+        } else if (now > 7 && now <= 11) {
+            text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
+        } else if (now > 11 && now <= 14) {
+            text = '中午了，工作了一个上午，现在是午餐时间！';
+        } else if (now > 14 && now <= 17) {
+            text = '午后很容易犯困呢，今天的运动目标完成了吗？';
+        } else if (now > 17 && now <= 19) {
+            text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~';
+        } else if (now > 19 && now <= 21) {
+            text = '晚上好，今天过得怎么样？';
+        } else if (now > 21 && now <= 23) {
+            text = '已经这么晚了呀，早点休息吧，晚安~';
+        } else {
+            text = '嗨~ 快来逗我玩吧！';
+        }
+    } else {
+        if(document.referrer !== ''){
+            var referrer = document.createElement('a');
+            referrer.href = document.referrer;
+            var domain = referrer.hostname.split('.')[1];
+            if (window.location.hostname == referrer.hostname) {
+                text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+            } else if (domain == 'baidu') {
+                text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
+            } else if (domain == 'so') {
+                text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
+            } else if (domain == 'google') {
+                text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+            } else {
+                text = 'Hello! 来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友';
+            }
+        } else {
+            text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+        }
+    }
+    showMessage(text, 6000);
+})();
+
+//window.hitokotoTimer = window.setInterval(showHitokoto,30000);
+/* 检测用户活动状态，并在空闲时 定时显示一言 */
+var getActed = false;
+window.hitokotoTimer = 0;
+var hitokotoInterval = false;
+
+$(document).mousemove(function(e){getActed = true;}).keydown(function(){getActed = true;});
+setInterval(function() { if (!getActed) ifActed(); else elseActed(); }, 1000);
+
+function ifActed() {
+    if (!hitokotoInterval) {
+        hitokotoInterval = true;
+        hitokotoTimer = window.setInterval(showHitokoto, 30000);
+    }
+}
+
+function elseActed() {
+    getActed = hitokotoInterval = false;
+    window.clearInterval(hitokotoTimer);
+}
+
+
+function showMessage(text, timeout, flag){
+    if(flag || sessionStorage.getItem('waifu-text') === '' || sessionStorage.getItem('waifu-text') === null){
+        if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
+        //console.log(text);
+        
+        if(flag) sessionStorage.setItem('waifu-text', text);
+        
+        $('.waifu-tips').stop();
+        $('.waifu-tips').html(text).fadeTo(200, 1);
+        if (timeout === undefined) timeout = 5000;
+        hideMessage(timeout);
+    }
+}
+
+function hideMessage(timeout){
+    $('.waifu-tips').stop().css('opacity',1);
+    if (timeout === undefined) timeout = 5000;
+    window.setTimeout(function() {sessionStorage.removeItem('waifu-text')}, timeout);
+    $('.waifu-tips').delay(timeout).fadeTo(200, 0);
+}
+
